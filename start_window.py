f697199f33dc8fc5d51b9265f75581979e3caab3
Dima <grmgorer@gmail.com>
2024-12-20 16:53:52 +0300

    Дима сделал все

---
+++ start_window.py
@@ -0,0 +1,52 @@
+import pygame
+import sys
+
+
+class StartWindow:
+    def __init__(self, size):
+        pygame.init()
+        self.screen = pygame.display.set_mode((size[0], size[1]))
+        pygame.display.set_caption("Adventures with mini games")
+        self.font = pygame.font.Font(None, 30)
+        self.button_font = pygame.font.Font(None, 48)
+
+        self.start_button = pygame.Rect(300, 300, 200, 100)
+        self.title_game = "Adventures with mini games"
+
+    def draw(self):
+
+        # Рисуем овальный фон вокруг кнопки
+        pygame.draw.ellipse(self.screen, (0, 0, 150), self.start_button.inflate(40, 40))
+        pygame.draw.ellipse(self.screen, (0, 0, 200), self.start_button.inflate(20, 20))
+        pygame.draw.ellipse(self.screen, (0, 0, 255), self.start_button.inflate(0, 0))
+        button_text = self.button_font.render("Начать игру", True, (255, 255, 255))
+        self.screen.blit(button_text, (self.start_button.x, self.start_button.y + 30))
+
+        # Отображаем текст
+        title_game = pygame.font.Font(None, 74).render(self.title_game, True, (255, 255, 255))
+        self.screen.blit(title_game, (80, 100))
+
+        pygame.display.flip()
+
+    def run(self):
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        if self.start_button.collidepoint(event.pos):
+                            running = False
+
+            self.draw()
+
+
+pygame.init()
+size = width, height = 830, 900
+screen = pygame.display.set_mode(size)
+
+StartWindow(size).run()
+pygame.quit()
