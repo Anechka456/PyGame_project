983727ca0fc46e286af8985822ac502bf29545cf
Dima <grmgorer@gmail.com>
2025-01-28 00:07:09 +0300

    Revert "соед."
    This reverts commit 8a016fe033cf8b4bdc21e1973489c7c13f15cc38.

--- regisrt_window.py
+++ regisrt_window.py
@@ -4,19 +4,13 @@ import sys
 # Инициализация Pygame
 pygame.init()
 
-proverka_abs = ['qwe', 'wer', 'ert', 'rty', 'tyu', 'yui', 'uio', 'iop', 'asd', 'sdf', 'dfg',
-                'fgh', 'ghj', 'hjk', 'jkl', 'zxc', 'xcv', 'cvb', 'vbn', 'bnm', 'йцу', 'цук',
-                'уке', 'кен',
-                'енг', 'нгш', 'гшщ', 'шщз', 'щзх', 'зхъ', 'фыв', 'ыва', 'вап', 'апр', 'про',
-                'рол', 'олд',
-                'лдж', 'джэ', 'ячс', 'чсм', 'сми', 'мит', 'ить', 'тьб', 'ьбю', 'жэё']
 # Цвета
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GRAY = (200, 200, 200)
 
 # Размеры окна
-WIDTH, HEIGHT = 900, 800
+WIDTH, HEIGHT = 400, 300
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Регистрация")
 
@@ -26,60 +20,13 @@ font = pygame.font.Font(None, 36)
 # Переменные для хранения вводимых данных
 username = ''
 password = ''
-input_box1 = pygame.Rect(WIDTH / 2 - 100, 100, 200, 40)
-input_box2 = pygame.Rect(WIDTH / 2 - 100, 170, 200, 40)
-login = pygame.Rect(50, 50, 50, 50)
+input_box1 = pygame.Rect(100, 100, 200, 40)
+input_box2 = pygame.Rect(100, 170, 200, 40)
 active1 = False
 active2 = False
-active3 = False
 color1 = GRAY
 color2 = GRAY
-color3 = GRAY
 
-def proverka(password, name):
-    try:
-        password = ''.join(password.split())
-        if name == '' and password == '':
-            Error('Ведите Name and password')
-        elif name == '':
-            Error('Ведите Name')
-        elif password == '':
-            Error('Ведите password')
-        elif len(password) < 9:
-            raise ValueError
-        elif password.isdigit():
-            raise ValueError
-        elif password.isalpha():
-            raise ValueError
-        elif password.islower():
-            raise ValueError
-        elif password.isupper():
-            raise ValueError
-        for i in proverka_abs:
-            if i in password.lower():
-                raise ValueError
-        flag_number = False
-        flag_lower = False
-        flag_upper = False
-        for letter in password:
-            if letter.isdigit():
-                flag_number = True
-            if letter.isupper():
-                flag_upper = True
-            if letter.islower():
-                flag_lower = True
-        if flag_lower and flag_upper and flag_number:
-            return True
-        raise ValueError
-    except Exception:
-        Error('Неверный пороль')
-
-
-def Error(text):
-    print(text) #Создвние менюшки ошибки
-def Login(active1, active2):
-    if proverka(password, username):
-        print('занесение в б д')
 # Основной игровой цикл
 while True:
     for event in pygame.event.get():
@@ -99,22 +46,14 @@ while True:
             else:
                 active2 = False
 
-            if login.collidepoint(event.pos):
-                Login(active1, active2)
-                active3 = not active3
-            else:
-                active3 = False
-                Login(active1, active2)
-
             # Изменяем цвет поля ввода
             color1 = (255, 0, 0) if active1 else GRAY
             color2 = (255, 0, 0) if active2 else GRAY
-            color3 = (255, 0, 0) if active3 else GRAY
 
         if event.type == pygame.KEYDOWN:
             if active1:
                 if event.key == pygame.K_RETURN:
-                    Login(active1, active2)
+                    print(f'Username: {username}')
                 elif event.key == pygame.K_BACKSPACE:
                     username = username[:-1]
                 else:
@@ -122,7 +61,7 @@ while True:
 
             if active2:
                 if event.key == pygame.K_RETURN:
-                    Login(active1, active2)
+                    print(f'Password: {password}')
                 elif event.key == pygame.K_BACKSPACE:
                     password = password[:-1]
                 else:
@@ -133,12 +72,10 @@ while True:
     # Отрисовка полей ввода
     pygame.draw.rect(screen, color1, input_box1, 2)
     pygame.draw.rect(screen, color2, input_box2, 2)
-    pygame.draw.rect(screen, color3, login, 2)
 
     # Отображаем текст
     txt_surface1 = font.render(username, True, BLACK)
-    txt_surface2 = font.render(password, True, BLACK)
-    '''txt_surface2 = font.render('*' * len(password), True, BLACK)  # Скрываем пароль'''
+    txt_surface2 = font.render('*' * len(password), True, BLACK)  # Скрываем пароль
     screen.blit(txt_surface1, (input_box1.x + 5, input_box1.y + 5))
     screen.blit(txt_surface2, (input_box2.x + 5, input_box2.y + 5))
 
